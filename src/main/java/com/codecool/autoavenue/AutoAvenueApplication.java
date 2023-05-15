e7ce03593a12b06ffdfd2c62c22679487cad16bd
lojb <balazsloj02@gmail.com>
2023-05-15 14:43:50 +0200

    Setup project

---
+++ src/main/java/com/codecool/autoavenue/AutoAvenueApplication.java
@@ -0,0 +1,13 @@
+package com.codecool.autoavenue;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutoAvenueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutoAvenueApplication.class, args);
+	}
+
+}
