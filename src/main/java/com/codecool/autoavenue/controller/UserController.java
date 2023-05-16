537f8682bbc1941e97a79a9933ce3c4f5ee56b97
SzimkoBalazs <ubeats1995@gmail.com>
2023-05-16 16:27:14 +0200

    Implement UserController

---
+++ src/main/java/com/codecool/autoavenue/controller/UserController.java
@@ -0,0 +1,59 @@
+package com.codecool.autoavenue.controller;
+
+import com.codecool.autoavenue.model.User;
+import com.codecool.autoavenue.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+
+    @Autowired
+    UserService userService;
+
+    @GetMapping
+    public List<User> getAllUsers(){return userService.getAllUsers();
+    }
+
+    @PostMapping
+    public ResponseEntity<Void> addUser(@RequestBody User user){
+        userService.addUser(user);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    @GetMapping("/{userId}")
+    public ResponseEntity<User> getUserById(@PathVariable("userId") Long id) {
+        User user = userService.getUserById(id).get();
+        if (user != null) {
+            return ResponseEntity.ok(user);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @PutMapping("/{userId}")
+    public ResponseEntity<Void> updateUser(@PathVariable("userId") Long id, @RequestBody User newUser) {
+        User updatedUser = userService.getUserById(id).get();
+        if (updatedUser != null) {
+            userService.updateUser(id, newUser);
+            return new ResponseEntity<>(HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @DeleteMapping("/{userId}")
+    public ResponseEntity<Void> deleteUser(@PathVariable("userId") Long id) {
+        userService.deleteUser(id);
+        return ResponseEntity.noContent().build();
+    }
+
+
+}
