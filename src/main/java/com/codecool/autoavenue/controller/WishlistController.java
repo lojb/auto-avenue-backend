b3c5b6dcfb412114c919e2785716566d18214ea9
lojb <balazsloj02@gmail.com>
2023-05-16 17:08:21 +0200

    Add missing annotations to WishlistController class

--- src/main/java/com/codecool/autoavenue/controller/WishlistController.java
+++ src/main/java/com/codecool/autoavenue/controller/WishlistController.java
@@ -11,7 +11,8 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@Controller
+@RestController
+@RequestMapping("/wishlists")
 public class WishlistController {
     @Autowired
     private WishlistService wishlistService;
