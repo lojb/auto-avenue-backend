50b1a6c7e8edd589ef11f481b6814a032261d960
lojb <balazsloj02@gmail.com>
2023-05-16 10:10:34 +0200

    Refactor Advert entity to support messages

--- src/main/java/com/codecool/autoavenue/model/Advert.java
+++ src/main/java/com/codecool/autoavenue/model/Advert.java
@@ -4,6 +4,8 @@ import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 import lombok.*;
 
+import java.util.List;
+
 @Entity
 @Getter
 @Setter
@@ -20,4 +22,6 @@ public class Advert {
     private String description;
     @ManyToOne
     private User seller;
+    @OneToMany(mappedBy = "advert")
+    private List<Message> messages;
 }
