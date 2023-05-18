0b8fc59a036e7fc3cac34848c296bf3e78665d7b
lojb <balazsloj02@gmail.com>
2023-05-18 14:42:16 +0200

    Add JsonManagedReference annotation to connect with User entity

--- src/main/java/com/codecool/autoavenue/model/Advert.java
+++ src/main/java/com/codecool/autoavenue/model/Advert.java
@@ -1,5 +1,6 @@
 package com.codecool.autoavenue.model;
 
+import com.fasterxml.jackson.annotation.JsonManagedReference;
 import jakarta.persistence.*;
 import lombok.*;
 
@@ -21,6 +22,7 @@ public class Advert {
     private String description;
     private double price;
     @ManyToOne
+    @JsonManagedReference
     private User seller;
     @OneToMany(mappedBy = "advert")
     private List<Message> messages;
