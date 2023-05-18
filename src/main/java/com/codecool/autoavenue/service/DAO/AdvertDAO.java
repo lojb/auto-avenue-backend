ec0849dd3d16cde14aa336c882da99be7b7ff6a1
lojb <balazsloj02@gmail.com>
2023-05-18 14:43:39 +0200

    Add new method to find all adverts by user id

--- src/main/java/com/codecool/autoavenue/service/DAO/AdvertDAO.java
+++ src/main/java/com/codecool/autoavenue/service/DAO/AdvertDAO.java
@@ -1,7 +1,11 @@
 package com.codecool.autoavenue.service.DAO;
 
 import com.codecool.autoavenue.model.Advert;
+import com.codecool.autoavenue.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface AdvertDAO extends JpaRepository<Advert, Long> {
+    List<Advert> findBySellerId(Long id);
 }
