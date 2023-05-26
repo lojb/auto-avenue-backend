3bbeb5e8d07b8cd00c6324f025b458a409145ed4
lojb <balazsloj02@gmail.com>
2023-05-26 09:59:02 +0200

    Add User-Wishlist relation

--- src/main/java/com/codecool/autoavenue/model/User.java
+++ src/main/java/com/codecool/autoavenue/model/User.java
@@ -21,4 +21,6 @@ public class User {
     private UserRole role;
     @OneToMany(mappedBy = "sellerId")
     private List<Advert> adverts;
+    @OneToOne
+    private Wishlist wishlist;
 }
--- src/main/java/com/codecool/autoavenue/model/Wishlist.java
+++ src/main/java/com/codecool/autoavenue/model/Wishlist.java
@@ -16,6 +16,7 @@ public class Wishlist {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     @OneToOne
+    @JoinColumn(name = "user_id")
     private User user;
     @OneToMany
     private List<Advert> wishlistItems;
