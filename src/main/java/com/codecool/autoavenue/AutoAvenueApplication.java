b42005c18e266558e5f6c36bfc7bf961a3a152f8
lojb <balazsloj02@gmail.com>
2023-05-31 15:54:37 +0200

    Add EnableMethodSecurity annotation

--- src/main/java/com/codecool/autoavenue/AutoAvenueApplication.java
+++ src/main/java/com/codecool/autoavenue/AutoAvenueApplication.java
@@ -2,7 +2,9 @@ package com.codecool.autoavenue;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 
+@EnableMethodSecurity(securedEnabled = true)
 @SpringBootApplication
 public class AutoAvenueApplication {
 
