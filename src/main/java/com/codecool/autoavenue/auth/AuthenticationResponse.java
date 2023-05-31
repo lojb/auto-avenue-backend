1cab26542044068c068371acabd0b97e318d61fd
lojb <balazsloj02@gmail.com>
2023-05-31 15:09:23 +0200

    Add username, role, id to AuthenticationResponse

--- src/main/java/com/codecool/autoavenue/auth/AuthenticationResponse.java
+++ src/main/java/com/codecool/autoavenue/auth/AuthenticationResponse.java
@@ -1,5 +1,6 @@
 package com.codecool.autoavenue.auth;
 
+import com.codecool.autoavenue.model.enums.UserRole;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -12,5 +13,7 @@ import lombok.NoArgsConstructor;
 public class AuthenticationResponse {
 
     private String token;
-
+    private String username;
+    private UserRole role;
+    private Long userId;
 }
