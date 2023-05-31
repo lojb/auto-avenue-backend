d5c8f8a2093212fc9bdb157d0685bb6d4c7725a3
lojb <balazsloj02@gmail.com>
2023-05-31 10:18:03 +0200

    Implement UserDetails

--- src/main/java/com/codecool/autoavenue/model/User.java
+++ src/main/java/com/codecool/autoavenue/model/User.java
@@ -3,7 +3,11 @@ package com.codecool.autoavenue.model;
 import com.codecool.autoavenue.model.enums.UserRole;
 import jakarta.persistence.*;
 import lombok.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
+import java.util.Collection;
 import java.util.List;
 
 @Entity
@@ -11,7 +15,7 @@ import java.util.List;
 @Getter
 @Setter
 @NoArgsConstructor
-public class User {
+public class User implements UserDetails {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
@@ -23,4 +27,29 @@ public class User {
     private List<Advert> adverts;
     @OneToOne
     private Wishlist wishlist;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority(role.name()));
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 }
