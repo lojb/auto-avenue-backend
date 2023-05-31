8576ab50d487cdc634fed3c4c1de7ac719ddafcf
SzimkoBalazs <ubeats1995@gmail.com>
2023-05-31 10:17:50 +0200

    UserDetails interface implemented

--- src/main/java/com/codecool/autoavenue/model/User.java
+++ src/main/java/com/codecool/autoavenue/model/User.java
@@ -3,24 +3,55 @@ package com.codecool.autoavenue.model;
 import com.codecool.autoavenue.model.enums.UserRole;
 import jakarta.persistence.*;
 import lombok.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
+import java.util.Collection;
 import java.util.List;
 
 @Entity
+@Data
+@Builder
 @Table(name = "users")
-@Getter
-@Setter
+@AllArgsConstructor
 @NoArgsConstructor
-public class User {
+public class User implements UserDetails{
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String username;
     private String email;
     private String password;
+    @Enumerated(EnumType.STRING)
     private UserRole role;
     @OneToMany(mappedBy = "sellerId")
     private List<Advert> adverts;
     @OneToOne
     private Wishlist wishlist;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority(role.name()));
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 }
