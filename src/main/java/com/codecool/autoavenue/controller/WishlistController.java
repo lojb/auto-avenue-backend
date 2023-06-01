a75e03f6edc85c1c96f2e67bd6307a37d3ab3118
lojb <balazsloj02@gmail.com>
2023-06-01 13:09:04 +0200

    Implement deleteItemFromWishlist method

--- src/main/java/com/codecool/autoavenue/controller/WishlistController.java
+++ src/main/java/com/codecool/autoavenue/controller/WishlistController.java
@@ -7,7 +7,6 @@ import com.codecool.autoavenue.service.WishlistService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -23,29 +22,33 @@ public class WishlistController {
         return wishlistService.getAllWishlists();
     }
 
-    @GetMapping("/{userId}")
-    public Wishlist getWishlistByUserId(@PathVariable("userId") Long id) {
-        return wishlistService.getWishlistByUserId(id);
+    @GetMapping("/{id}")
+    public Wishlist getWishlistByUserId(@PathVariable("id") Long id) {
+        return wishlistService.getWishlistById(id);
     }
 
     @PostMapping
-    public ResponseEntity<Void> addWishlist(@RequestBody User user){
+    public ResponseEntity<Void> addWishlist(@RequestBody User user) {
         wishlistService.addWishlistForUser(user);
         return new ResponseEntity<>(HttpStatus.OK);
     }
 
     @PutMapping("/{id}")
-    public ResponseEntity<Void> updateWishlistById(@PathVariable("id") Long id, @RequestBody Advert advert){
-        Wishlist wishlistToUpdate = wishlistService.getWishlistById(id);
+    public ResponseEntity<Void> addToWishlist(@PathVariable("id") Long id, @RequestBody Advert advert) {
+        wishlistService.addAdvertToWishlist(id, advert);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
 
-        if (wishlistToUpdate != null) {
-            wishlistService.addAdvertToWishlist(id, advert);
-            return new ResponseEntity<>(HttpStatus.OK);
-        } else return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    @DeleteMapping("/delete-item")
+    public ResponseEntity<Void> deleteItemFromWishlist(@RequestParam(value = "item") Long itemId,
+                                                       @RequestParam(value = "wishlist") Long wishlistId) {
+        wishlistService.deleteWishlistItem(itemId, wishlistId);
+
+        return new ResponseEntity<>(HttpStatus.OK);
     }
 
     @DeleteMapping("/{id}")
-    public ResponseEntity<Void> deleteWishlistById(@PathVariable("id") Long id){
+    public ResponseEntity<Void> deleteWishlistById(@PathVariable("id") Long id) {
         Wishlist wishlistToDelete = wishlistService.getWishlistById(id);
 
         if (wishlistToDelete != null) {
